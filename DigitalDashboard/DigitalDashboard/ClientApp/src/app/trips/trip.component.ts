5c7cb17c1f01b82e7d137f69d46cd79c9fc84ac4
Morris <mnkosi@retrorabbit.co.za>
2019-02-12 00:27:39 +0200

    created test drive project setup

---
+++ DigitalDashboard/DigitalDashboard/ClientApp/src/app/trips/trip.component.ts
@@ -0,0 +1,11 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'trip',
+  templateUrl: './trip.component.html'
+})
+export class TripComponent {
+ 
+}
+
